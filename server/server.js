9affe3437642a0963a99923baeebf11876f7da21
Erik Grönlund <erik.gronlund@gmail.com>
2015-04-13 11:28:28 +0200

    render index ejs as default page

--- server/server.js
+++ server/server.js
@@ -6,11 +6,19 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+require("node-jsx").install(); 
+
 var port = process.env.PORT || 3000;
 var staticDir = path.resolve(__dirname + '/client');
 
+var React = require('react/addons');
+var App = React.createFactory(require('./components/App.js'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Server side default');
+  var reactLoginHtml = React.renderToString(App());
+  res.render('index.ejs', {reactContent: reactLoginHtml});
 });
 
 app.get('/login', function (req, res) {
