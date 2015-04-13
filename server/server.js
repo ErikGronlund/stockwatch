5730ebef1abafbf7d309131dd37ba52358819ecc
Erik Grönlund <erik.gronlund@gmail.com>
2015-04-13 10:53:59 +0200

    added default route

--- server/server.js
+++ server/server.js
@@ -9,11 +9,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 var staticDir = path.resolve(__dirname + '/client');
 
+app.get('/', function (req, res) {
+  res.send('Server side default');
+});
+
+app.get('/login', function (req, res) {
+  res.send('Login using google');
+});
+
 app.use(express.static(staticDir));
 app.listen(port);
 
 console.log('Serving ' + staticDir + '/ on http://localhost:' + port + '/');
 
-app.get('/login', function (req, res) {
-  res.send('Login using google');
-});
