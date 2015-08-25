6aa5513c36d4a78881f0975856479b27943d1420
Erik Grönlund <erik.gronlund@gmail.com>
2015-08-25 13:52:22 +0200

    added search stock symbol button

--- client/components/add-quote/AddQuote.js
+++ client/components/add-quote/AddQuote.js
@@ -2,14 +2,61 @@
 
 var React = require('react');
 
+var financeApi = require('../yahoo-finance-api');
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {showAddButton: false, searchResult: []};
+  },
+  searchSymbol: function (req) {
+    var that = this;
+    function onload () {
+      var json = JSON.parse(this.responseText);
+      that.setState({showAddButton: json.length > 0, searchResult: json});
+    }
+
+    function onerror() {
+      reject('failed searching symbol from server');
+    }
+
+    var searchValue = encodeURIComponent(document.getElementById('searchField').value);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = onload;
+    xhr.onerror = onerror;
+    xhr.open('get', '/searchsymbol?searchParam=' + searchValue, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send();
+  },
   render: function () {
+    var button;
+
+    var options = this.state.searchResult.map(function (data) {
+      return <option value={data.symbol}>{data.symbol}</option>
+    });
+
+    {
+      if (this.state.showAddButton) {
+        button =  <div>
+                    <form action="/addquote" method="post">
+                      Add stock to list:
+                      <select name="selectedSymbol">
+                        { options }
+                      </select>
+                      <button name="addQuote" type="submit" >Add</button>
+                    </form>
+                  </div>;
+      } else {
+        button =  <div>
+                      Search stock symbol: <input type="text" name="symbol" id="searchField"></input>
+                      <button name="searchSymbol" onClick={this.searchSymbol}>Search</button>
+                  </div>;
+      }
+    }
+
     return (
       <div>
-        <form action="/addquote" method="post">
-          Add symbol to list: <input type="text" name="symbol"></input>
-          <button name="addQuote" type="submit">Add</button>
-        </form>
+        { button }
       </div>
     );
   }
